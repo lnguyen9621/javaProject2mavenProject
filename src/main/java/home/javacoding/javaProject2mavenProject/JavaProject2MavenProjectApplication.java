e355ef8303de786a1c2e2807edb9d5025916c8cb
longnguyen <longnguyen@localhost>
2020-02-23 08:06:02 -0700

    commit converting from simple java project to maven project as following step: 1. Right click on the the project name: javaProject2mavenProject. 2. Select Congigure -> Convert to Maven Project.

---
+++ src/main/java/home/javacoding/javaProject2mavenProject/JavaProject2MavenProjectApplication.java
@@ -0,0 +1,13 @@
+package home.javacoding.javaProject2mavenProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProject2MavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProject2MavenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/javacoding/javaProject2mavenProject/JavaProject2MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package home.javacoding.javaProject2mavenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProject2MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
